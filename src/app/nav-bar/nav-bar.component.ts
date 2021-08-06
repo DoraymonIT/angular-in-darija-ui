dd024c60b3917aca22cebe886500c4465cbd6f28
DoraymonIT <53580861+DoraymonIT@users.noreply.github.com>
2021-08-06 15:23:43 +0100

    start with the navbar in mobile device

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -3,13 +3,18 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.sass']
+  styleUrls: ['./nav-bar.component.sass'],
 })
 export class NavBarComponent implements OnInit {
+  className: string = '';
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClickChange() {
+    if (this.className !== 'open') {
+      this.className = 'open';
+    } else {
+      this.className = '';
+    }
   }
-
 }
